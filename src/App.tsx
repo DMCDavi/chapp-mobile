f1caa33f13408101e3081ee4db0e64045c30dd7e
Davi Costa <davi.machado.costa@hotmail.com>
2020-12-14 16:20:59 -0300

    Creating private routes

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,15 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
 import Login from './pages/Login';
+import PrivateRoute from './routes/PrivateRoute';
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
+        <PrivateRoute path="/home" component={Home} exact={true} />
         <Route path="/login" component={Login} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/login" />} />
       </IonRouterOutlet>
