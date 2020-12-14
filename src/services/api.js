d887aac796b3c240e069008ef542c126234245b3
Davi Costa <davi.machado.costa@hotmail.com>
2020-12-14 18:14:46 -0300

    Adding bearer token to axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,19 @@
 import axios from "axios";
-// import { getToken } from '../utils/storage'
+import { getToken } from "../utils/storage";
 
 // To make it safier we need to put this string in .env archive
 const url = "https://stark-wildwood-30574.herokuapp.com";
 
 const api = axios.create({
-  baseURL: url,
-//   withCredentials: true,
+  baseURL: url
 });
 
-// api.interceptors.request.use(async (config) => {
-//   const token = getToken();
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+api.interceptors.request.use(async (config) => {
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
